d24aab2b86194be6dac2e5da0a1e85b284e3f954
Lushi Li <lushili06@gmail.com>
2016-11-28 18:49:44 -0500

    flesh out createStore()

--- src/createStore.js
+++ src/createStore.js
@@ -1,9 +1,27 @@
-/* Inspired by Redux. Super simple API for updating and fetchin
+/* Based on Redux. Super simple API for updating and fetching
  * application state. Reasoning is to keep application state separate
  * from application logic.
 */
-const createStore = () => {
-  return {};
+const createStore = (reducer, initialState={}) => {
+  if (typeof reducer !== 'function') {
+    throw new Error('reducer is expected to be a function.');
+  }
+
+  _state = initialState;
+
+  return {
+    getState: () => {
+      return _state;
+    },
+
+    dispatch: (action) => {
+      if (typeof action.type === 'undefined') {
+        throw new Error('action must have type property.');
+      }
+
+      _state = reducer(_state, action);
+    }
+  };
 }
 
 module.exports = createStore;
--- test/createStore.js
+++ test/createStore.js
@@ -1,7 +1,58 @@
-const chai = require('chai');
+const chai = require("chai");
 const expect = chai.expect;
 
-const createStore = require('../src/createStore.js');
+const createStore = require("../src/createStore.js");
 
-describe('createStore', () => {
+describe("createStore", () => {
+  describe("parameters", () => {
+    it("expects parameter 'reducer' to be a function", () => {
+      let fn = () => {createStore();}
+      expect(fn).to.throw(Error);
+    });
+
+    it("defaults initialState to empty object if no argument is passed", () => {
+      let store = createStore(()=>{});
+      let state = store.getState();
+      expect(state).to.deep.equal({});
+    });
+  });
+
+  describe("API", () => {
+    describe("#getState", () => {
+      it("returns the current state object", () => {
+        let initialState = {"foo": "bar"};
+        let store = createStore(()=>{}, initialState);
+        let state = store.getState();
+        expect(state).to.deep.equal(initialState);
+      })
+    });
+
+    describe("#dispatch", () => {
+      it("expects parameter 'action' to be an object literal with own property 'type'", () => {
+        let store = createStore(()=>{});
+        let fn = () => {store.dispatch({})};
+
+        expect(fn).to.throw(Error);
+      })
+    })
+  });
+
+  describe("data flow", () => {
+    it("should update data by the reducer depending on the dispatched action", () => {
+      let reducer = (state = {}, action) => {
+        switch (action.type) {
+          case 'INCREMENT':
+            return Object.assign({}, state, {count: state.count + 1});
+          case 'DECREMENT':
+            return Object.assign({}, state, {count: state.count - 1});
+          default:
+            return state;
+        }
+      }
+
+      let store = createStore(reducer, {count: 0});
+      store.dispatch({type: 'INCREMENT'});
+      expect(store.getState().count).to.equal(1);
+    });
+  });
 });
