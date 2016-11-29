69ef722468c40f2b3dc26abfba8383f02baa66b3
Lushi Li <lushili06@gmail.com>
2016-11-28 20:41:26 -0500

    draw border

--- src/Snake.js
+++ src/Snake.js
@@ -5,23 +5,48 @@ class Snake {
 
   init() {
     let ctx = this._initCanvas();
+    this._draw(ctx);
   }
 
   _initCanvas() {
     const elId = 'snake-canvas';
-    const state = this.store.getState();
     let canvas = window.document.getElementById(elId);
     if (!canvas) {
-      canvas = window.document.createElement('div');
+      canvas = window.document.createElement('canvas');
       canvas.setAttribute('id', elId);
       window.document.body.appendChild(canvas);
     }
+    const state = this._getState();
     const width = state.WIDTH * state.TILE_SIZE;
     const height = state.HEIGHT * state.TILE_SIZE;
     canvas.setAttribute('width', width);
     canvas.setAttribute('height', height);
 
     return canvas.getContext('2d');
+  }
+
+  _draw(ctx) {
+    ctx.save();
+    this._drawBorder(ctx);
+    ctx.restore();
+  }
+
+  _drawBorder(ctx) {
+    const tile_size = this._getState().TILE_SIZE;
+    const width = this._getState().WIDTH * tile_size;
+    const height = this._getState().HEIGHT * tile_size;
+    const offset = tile_size * (3/4); // offset is center of where line will be drawn
+
+    ctx.save();
+    ctx.lineWidth = tile_size / 2;
+    ctx.rect(offset, offset, width - (offset * 2), height - (offset * 2));
+    ctx.stroke();
+    ctx.restore();
+  }
+
+  // convenience function, to save some typing
+  _getState() {
+    return this.store.getState();
   }
 }
 
