7f950648fd444a8884381918300cbd4995260eb9
Lushi Li <lushili06@gmail.com>
2016-11-28 19:04:05 -0500

    scaffold top-level Snake class and test

---
+++ test/Snake.js
@@ -0,0 +1,10 @@
+const chai = require("chai");
+const expect = chai.expect;
+
+const Snake = require("../src/Snake.js");
+
+describe("Snake", () => {
+  it("should write tests", () => {
+    expect(false).to.equal(true);
+  })
+});
